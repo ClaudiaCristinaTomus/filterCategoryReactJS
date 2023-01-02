28f64d0830cc2b61a0799da53893d24437d2b821
ClaudiaCristinaTomus <tomus_clau@yahoo.com>
2023-01-02 19:18:31 +0200

    add files

---
+++ src/components/Categories.jsx
@@ -0,0 +1,25 @@
+const Categories =[
+    {
+        id:1,
+        category:'Men',
+        title:'Men Product',
+        price:21.00,
+        image:"/images/man.jpg"
+    },
+    {
+        id:2,
+        category:'Women',
+        title:'Women Product',
+        price:31.00,
+        image:"/images/women.jpg"
+    },
+    {
+        id:3,
+        category:'Kids',
+        title:'Kids Product',
+        price:11.00,
+        image:"/images/kids.jpg"
+    }
+]
+
+export default Categories;
